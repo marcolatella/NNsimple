d2512903d98e9ee966e54bbaaf1acc1fe87a04bf
Marco Latella <mrclatella@gmail.com>
2022-05-14 23:13:46 +0200

    Added comments

--- nnsimple/predictors/predictor.py
+++ nnsimple/predictors/predictor.py
@@ -15,6 +15,7 @@ class Predictor(pl.LightningModule):
         self.val_metric = None
         self.test_metric = None
 
+        # Checking if the metrics is a dictionary.
         if isinstance(metrics, dict):
             # TODO check if keys in 'metrics' exist ('train_metrics', ...)
             self.train_metric = metrics['train_metrics']
@@ -23,6 +24,14 @@ class Predictor(pl.LightningModule):
             # TODO else condition
 
     def training_step(self, batch, batch_index):
+        """
+        We take a batch of data, pass it through the model, calculate the loss, calculate the metrics, and log the loss and
+        metrics
+
+        :param batch: The input batch
+        :param batch_index: The index of the batch in the current epoch
+        :return: The loss value
+        """
         x, y = batch
         logits = self.model(x)
         train_loss = self.loss_fn(logits, y)
@@ -32,6 +41,13 @@ class Predictor(pl.LightningModule):
         return train_loss
 
     def validation_step(self, batch, batch_index):
+        """
+        We pass in a batch of data, and the batch index, and we return the validation loss
+
+        :param batch: The batch of data that was passed to the validation_step function
+        :param batch_index: The index of the batch in the current epoch
+        :return: The validation loss is being returned.
+        """
         x, y = batch
         logits = self.model(x)
         val_loss = self.loss_fn(logits, y)
@@ -41,6 +57,14 @@ class Predictor(pl.LightningModule):
         return val_loss
 
     def test_step(self, batch, batch_index):
+        """
+        The test_step function takes the test data and calculates the loss and
+        metrics
+
+        :param batch: The batch of data that was passed to the test_step function
+        :param batch_index: The index of the batch in the current epoch
+        :return: The test loss is being returned.
+        """
         x, y = batch
         logits = self.model(x)
         test_loss = self.loss_fn(logits, y)
@@ -49,6 +73,10 @@ class Predictor(pl.LightningModule):
         return test_loss
 
     def configure_optimizers(self):
+        """
+        The function takes in the optimizer that was defined in the init function and returns it
+        :return: The optimizer
+        """
         optimizer = self.optimizer
         return optimizer
 
